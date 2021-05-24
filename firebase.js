8197497e22cbd7c9d348268641ee3f613560ec59
trickymind1324 <trickymind1224.com>
2021-05-24 12:53:22 +0530

    updated files

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBAYDAV3p_kVhhdpoaFMYzAv1YVUbPiqaM",
+    authDomain: "clone-18-f7be5.firebaseapp.com",
+    projectId: "clone-18-f7be5",
+    storageBucket: "clone-18-f7be5.appspot.com",
+    messagingSenderId: "39060735486",
+    appId: "1:39060735486:web:21883b2dedc20dfdc32caf",
+    measurementId: "G-Z37YL2HW30"
+  };
--- src/components/Product.js
+++ src/components/Product.js
@@ -3,16 +3,34 @@ import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
+
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Product({id, title, price, description, category, image}) {
-const [rating] = useState(
+
+    const dispatch = useDispatch();
+    const [rating] = useState(
     Math.floor(Math.random()*(MAX_RATING - MIN_RATING + 1)) + MIN_RATING
 );
 
-const [hasPrime] = useState(Math.random() <0.5 )
+const [hasPrime] = useState(Math.random() <0.5 );
+const addItemToBasket = () => {
+    const product = {
+        id,
+        title,
+        price,
+        rating,
+        description, 
+        category, 
+        image,
+        hasPrime,
+    };
+    dispatch(addToBasket(product));
+};
 
     return (
         <div className="relative flex flex-col m-5 bg-white z-30 p-10">
@@ -32,7 +50,7 @@ const [hasPrime] = useState(Math.random() <0.5 )
 
              <p className="text-xs my-2 line-clamp-2">{description}</p>
              <div className="mb-5">
-                 <Currency quantity={price} currency="GBP"/>
+                 <Currency quantity={price} currency="INR"/>
              </div>
             {hasPrime && (
                 <div className="flex items-center space-x-2 -mt-5">
@@ -40,7 +58,7 @@ const [hasPrime] = useState(Math.random() <0.5 )
                     <p className="text-xs text-gray-500">FREE Delivery</p>
                 </div>
             )}
-            <button className="mt-auto button">Add to Basket</button>
+            <button onClick={addItemToBasket} className="mt-auto button">Add to Basket</button>
             </div>
     );
 }
